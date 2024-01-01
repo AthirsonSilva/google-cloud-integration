98869f30c7d6b976256b372f970556aa29a8f920
Athirson <athirsonarceus@gmail.com>
2024-01-01 14:54:53 -0300

    first commit

---
+++ src/main/java/com/googlecloudintegration/GoogleCloudIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.googlecloudintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googlecloudintegration/GoogleCloudIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googlecloudintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloudIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
